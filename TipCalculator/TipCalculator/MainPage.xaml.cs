66558e479306f74411b42dd9f0717ce2fcea47ae
Diego <dsanthony997@gmail.com>
2018-01-16 17:12:28 -0500

    Completed

--- TipCalculator/TipCalculator/MainPage.xaml.cs
+++ TipCalculator/TipCalculator/MainPage.xaml.cs
@@ -1,17 +1,28 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace TipCalculator
 {
-	public partial class MainPage : ContentPage
+    public partial class MainPage : ContentPage
 	{
 		public MainPage()
 		{
 			InitializeComponent();
+            btnCalcular.Clicked += BtnCalcular_Clicked;
 		}
-	}
+
+        private void BtnCalcular_Clicked(object sender, System.EventArgs e)
+        {
+            var total = decimal.Parse(Total.Text);
+            var propina = decimal.Parse(Propina.Text);
+            var numPersonas = int.Parse(NumPersonas.Text);
+            var totalPropina = total * propina / 100;
+            lblPropina.Detail = totalPropina.ToString("C");
+            var mtoTotal = totalPropina + total;
+            lblTotal.Detail = mtoTotal.ToString("C");
+            var propinaPorPersona = totalPropina / numPersonas;
+            lblPropinaPorPersona.Detail = propinaPorPersona.ToString("C");
+            var totalPorPersona = mtoTotal / numPersonas;
+            lblTotalPorPersona.Detail = totalPorPersona.ToString("C");
+        }
+    }
 }

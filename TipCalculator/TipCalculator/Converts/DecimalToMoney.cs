36fcf9ce3a9460e3db2360a02d0150f3f1e682b0
Diego <dsanthony997@gmail.com>
2018-01-18 01:59:57 -0500

    ValueConverters

---
+++ TipCalculator/TipCalculator/Converts/DecimalToMoney.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TipCalculator.Converts
+{
+    class DecimalToMoney : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var valor = (decimal)value;
+            return valor.ToString("C");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var valor = (string)value;
+            return valor.Remove(0);
+        }
+    }
+}
